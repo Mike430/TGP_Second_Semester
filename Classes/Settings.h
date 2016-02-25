1c993e87af90504ff7a071210f365068524f4811
benjim <bencrawford101@gmail.com>
2016-02-25 16:30:53 +0000

    Added Settings.h

---
+++ Classes/Settings.h
@@ -0,0 +1,9 @@
+namespace Settings
+{
+	const float gravity = 15 * 60;//was 15 per frame at ~60fps, now framerate independant;
+	const float dropRate = -1;
+	const float horizontalSpeed = 0;
+	const float targetSpawnRate = 0;
+	const float targetSizeCommon = 0;
+	const float targetSizeRare = targetSizeCommon;
+}
