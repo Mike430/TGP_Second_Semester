03ccced07efb6783e8876b96dca0d98f4ef19ae9
benjim <bencrawford101@gmail.com>
2016-02-25 16:59:30 +0000

    Added ball horizontal speed setting

--- Classes/Settings.h
+++ Classes/Settings.h
@@ -4,7 +4,7 @@ namespace Settings
 {
 	const float gravity = 15 * 60; // was 15 per frame at ~60fps, now framerate independant;
 	const float dropRate = -1; // Drops per second, -1 means only drop when previous ball is destroyed
-	const float horizontalSpeed = 0;
+	const float horizontalSpeed = 1500;
 	const float targetSpawnRate = 0;
 	const float targetSizeCommon = 0;
 	const float targetSizeRare = targetSizeCommon;
