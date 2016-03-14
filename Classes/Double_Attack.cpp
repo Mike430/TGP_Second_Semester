2870a3b81bc0ea96177aa5106bbca919c5989d28
unknown <b015698c@K106-16.student.staffs.ac.uk>
2016-03-14 18:39:58 +0000

    Ollie_b015698c Added Double_Attack Class

---
+++ Classes/Double_Attack.cpp
@@ -0,0 +1,11 @@
+#include "Double_Attack.h"
+
+
+Double_Attack::Double_Attack()
+{
+}
+
+
+Double_Attack::~Double_Attack()
+{
+}
---
+++ Classes/Double_Attack.h
@@ -0,0 +1,8 @@
+#pragma once
+class Double_Attack
+{
+public:
+	Double_Attack();
+	~Double_Attack();
+};
+
