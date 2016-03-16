5312427f20d654d6d775f10039b73f453c2b7d18
benjim <bencrawford101@gmail.com>
2016-03-16 11:42:17 +0000

    Updated android build to automatically compile all .cpp files in Classes/ folder

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,17 +10,9 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp \
-		   ../../Classes/Ball.cpp \
-                   ../../Classes/BallDispencer.cpp \
-		   ../../Classes/BallManager.cpp \
-                   ../../Classes/Game_Scene.cpp \
-		   ../../Classes/GameManager.cpp \
-                   ../../Classes/Player.cpp \
-		   ../../Classes/Score_Scene.cpp \
-		   ../../Classes/Target.cpp \
+LOCAL_SRC_FILES := hellocpp/main.cpp
+FILE_LIST := $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp)
+LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
